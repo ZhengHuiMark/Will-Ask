a2dc43eb7518bed3c1513a8f4a3fc8016d798cca
ZhengHuiMark <593394100@qq.com>
2017-02-08 11:02:17 +0800

    创建项目

---
+++ HuiWen/HuiWen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HuiWen
+//
+//  Created by 郑晖 on 2017/2/8.
+//  Copyright © 2017年 郑晖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HuiWen/HuiWen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HuiWen
+//
+//  Created by 郑晖 on 2017/2/8.
+//  Copyright © 2017年 郑晖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
